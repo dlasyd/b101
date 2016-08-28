2d72f53d046af0b74ed5ae0a7553b37eff0aa9f6
Dlasyd <malevanyy@gmail.com>
2016-08-28 10:23:18 +0300

    hotfix

--- b111/settings.py
+++ b111/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'b101develop',
         'USER': 'postgres',
         'PASSWORD': '33rjhjds',
         'HOST': '127.0.0.1',
